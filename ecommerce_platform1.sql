c94655a511c27bd900cf87c91ec22b248c67c72c
Alaa-Eissa <alaaeissa859@gmail.com>
2024-12-26 20:31:38 +0200

    ecommerce_platform1.sql

--- ecommerce_platform1.sql
+++ ecommerce_platform1.sql
@@ -20,6 +20,7 @@ name VARCHAR(100) NOT NULL ,
 description TEXT ,
 price DECIMAL(10, 2) NOT NULL ,
 stock_quantity INT NOT NULL ,
+ user_id INT NOT NULL ,
 category_id INT,
  foreign key (user_id) references Users (user_id),
 foreign key (category_id) references categories(category_id)
